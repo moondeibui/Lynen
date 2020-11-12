a2b53cb0a85cd286fc1e459f20ba3cc5a446cba6
moon <anyeong1@outlook.com>
2020-11-12 11:22:52 +0800

    Added ProductPage and PaymentPage 2

---
+++ app/src/main/java/com/example/myapplication/Product.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_btn);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
