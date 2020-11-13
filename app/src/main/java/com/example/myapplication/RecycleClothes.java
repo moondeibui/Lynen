3dcb2795edd712a96162f5754af603cb031a04b6
Bryan Eeo <bryan.eeo2@gmail.com>
2020-11-13 23:22:50 +0800

    Updated Home Page, added Icons to all the buttons. Changed some background as well. *NOTE: RecycleClothes Page still not done.

--- app/src/main/java/com/example/myapplication/RecycleClothes.java
+++ app/src/main/java/com/example/myapplication/RecycleClothes.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import android.Manifest;
@@ -12,22 +13,37 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class RecycleClothes extends AppCompatActivity {
 
+    Button test;
+    CardView cardView;
     ImageButton cameraButton;
-    Dialog photoDialog;
     ImageView photoImgView, closePopUp;
-    //Bitmap bitmap;
+    Bitmap bitmap;
+    CameraPopUpDialog cameraPopUpDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle_clothes);
 
+        test = findViewById(R.id.test_pop_up_btn);
+
         cameraButton = findViewById(R.id.camera_btn);
+        cardView = findViewById(R.id.recycle_clothes_cardview);
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraPopUpDialog = new CameraPopUpDialog(RecycleClothes.this, bitmap);
+                cameraPopUpDialog.show();
+            }
+        });
 
         if (ContextCompat.checkSelfPermission(RecycleClothes.this, Manifest.permission.CAMERA)
         != PackageManager.PERMISSION_GRANTED){
@@ -40,14 +56,12 @@ public class RecycleClothes extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        photoDialog = new Dialog(this);
 
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 1000);
-                //showPhotoPopUp(bitmap);
+                startActivityForResult(intent, 100);
             }
         });
     }
@@ -55,40 +69,28 @@ public class RecycleClothes extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == 100) {
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-            photoDialog = new Dialog(this);
+        if (requestCode == 100) {
+            bitmap = (Bitmap) data.getExtras().get("data");
 
-            photoDialog.setContentView(R.layout.camera_pop_up_dialog);
+            Toast.makeText(this, "Hi", Toast.LENGTH_SHORT).show();
 
-            photoImgView = findViewById(R.id.photo_imgview);
-            closePopUp = findViewById(R.id.close_pop_up_imgview);
+            /*cameraPopUpDialog = new CameraPopUpDialog(this, bitmap);
+            cameraPopUpDialog.show();*/
 
+            /*photoImgView = findViewById(R.id.recycle_clothes_photo_imgview);
+            closePopUp = findViewById(R.id.recycle_clothes_close_pop_up_imgview);
+
+            //cardView.setVisibility(View.VISIBLE);
             photoImgView.setImageBitmap(bitmap);
 
             closePopUp.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    photoDialog.dismiss();
+                    cardView.setVisibility(View.INVISIBLE);
                 }
-            });
+            });*/
 
         }
     }
 
-    public void showPhotoPopUp(Bitmap bitmap) {
-        photoDialog.setContentView(R.layout.camera_pop_up_dialog);
-
-        photoImgView = findViewById(R.id.photo_imgview);
-        closePopUp = findViewById(R.id.close_pop_up_imgview);
-
-        //photoImgView.setImageBitmap(bitmap);
-
-        closePopUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                photoDialog.dismiss();
-            }
-        });
-    }
 }
